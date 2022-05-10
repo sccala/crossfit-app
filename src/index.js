e6c64a048a56f9fd3eae8188f788323d156f5000
Chase <sccala18@gmail.com>
2022-05-10 14:11:46 -0700

    hook up v1workouts router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
-
+const v1WorkoutRouter = require('./v1/routes/workoutRoutes')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res)=> {
-    res.send('<h1>its working</h1>')
-})
+// app.get('/', (req, res)=> {
+//     res.send('<h1>its working</h1>')
+// })
 
-app.listen(PORT, ()=>{
-    console.log(`API is listening on port ${PORT}`)
+app.use('/api/v1/workouts', v1WorkoutRouter)
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`)
 })
