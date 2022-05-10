0e8840880337b520094a188cb36fef1895cef1ed
Chase <sccala18@gmail.com>
2022-05-10 16:01:24 -0700

    added services

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const v1WorkoutRouter = require('./v1/routes/workoutRoutes')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -6,7 +7,7 @@ const PORT = process.env.PORT || 3000
 // app.get('/', (req, res)=> {
 //     res.send('<h1>its working</h1>')
 // })
-
+app.use(bodyParser.json())
 app.use('/api/v1/workouts', v1WorkoutRouter)
 
 app.listen(PORT, () => {
