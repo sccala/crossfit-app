e719b94e684c286ee81be2b393064bc15416534c
Chase <sccala18@gmail.com>
2022-05-11 17:06:48 -0700

    added record controller

--- src/controllers/recordController.js
+++ src/controllers/recordController.js
@@ -1,5 +1,14 @@
+const recordService = require('../services/recordService')
+
 const getRecordForWorkout = (req, res) => {
-  return
+  try {
+    const allRecords = recordService.getRecordForWorkout
+    res.send({ status: 'OK', data: { allRecords } })
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: 'FAILED', data: { error: error?.message || error } })
+  }
 }
 
 module.exports = {
