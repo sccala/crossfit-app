475d48553c87b808f236402f00c72a3853ecc33c
Chase <sccala18@gmail.com>
2022-05-11 17:22:35 -0700

    added api cache

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const v1WorkoutRouter = require('./v1/routes/workoutRoutes')
+
 const app = express()
+const cache = apicache.middleware
 const PORT = process.env.PORT || 3000
+const apicache = require('apicache')
+
 
 // app.get('/', (req, res)=> {
 //     res.send('<h1>its working</h1>')
 // })
 app.use(bodyParser.json())
+app.use(cache('2 minutes'))
 app.use('/api/v1/workouts', v1WorkoutRouter)
 
 app.listen(PORT, () => {
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const apicache = require('apicache')
 const router = express.Router()
+const cache = apicache.middleware
 const workoutController = require('../../controllers/workoutController')
 const recordController = require('../../controllers/recordController')
 
+router.get('/', cache('2 minutes'), workoutController.getAllWorkouts)
 router.get('/', workoutController.getAllWorkouts)
 router.get('/:workoutId', workoutController.getOneWorkout)
 router.get('/:workoutId/records', recordController.getRecordForWorkout)
