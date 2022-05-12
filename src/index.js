9a4bc446a048b58d7cf9e4099c863ca081b2e6a5
Chase <sccala18@gmail.com>
2022-05-11 20:27:11 -0700

    added api docs and swagger

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const v1WorkoutRouter = require('./v1/routes/workoutRoutes')
+const { swaggerDocs: V1SwaggerDocs } = require('./v1/swagger')
 
 const app = express()
-const cache = apicache.middleware
 const PORT = process.env.PORT || 3000
-const apicache = require('apicache')
 
-
-// app.get('/', (req, res)=> {
-//     res.send('<h1>its working</h1>')
-// })
 app.use(bodyParser.json())
-app.use(cache('2 minutes'))
 app.use('/api/v1/workouts', v1WorkoutRouter)
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`)
+  V1SwaggerDocs(app, PORT)
 })
